ec8f145bcce62009f06598118c202cb5b09e7e87
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-02-23 20:48:27 +0000

    ...

--- routes/tickets.php
+++ routes/tickets.php
@@ -8,20 +8,22 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::put('/tickets/{ticket}/assign', [TicketController::class, 'assign'])->name('tickets.assign');
 });
 
-
 Route::middleware(['auth', 'role:client'])->group(function () {
     Route::get('/client/tickets', [TicketController::class, 'clientTickets'])->name('client.tickets.index');
     Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
-    Route::post('/tickets/store', [TicketController::class, 'store'])->name('tickets.store');
+    Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
 
     Route::get('/tickets/{ticket}/edit', [TicketController::class, 'edit'])->name('tickets.edit');
     Route::put('/tickets/{ticket}', [TicketController::class, 'update'])->name('tickets.update');
 
-    Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])->name('ticket.destroy');
+    Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy');
+    Route::post('/tickets/{ticket}/close', [TicketController::class, 'close'])->name('tickets.close');
 });
 
 Route::middleware(['auth', 'role:agent'])->group(function () {
     Route::get('/agent/tickets', [TicketController::class, 'agentAssignedTickets'])->name('agent.tickets.index');
+    Route::post('/tickets/{ticket}/status/{newStatus}', [TicketController::class, 'changeStatus'])->name('tickets.changeStatus');
 });
-Route::post('/tickets/{ticket}/status/{newStatus}', [TicketController::class, 'changeStatus'])->name('tickets.changeStatus');
-Route::get('/tickets/{ticket}/', [TicketController::class, 'show'])->name('ticket.details');
+
+// Show Ticket (must be last to avoid conflicts)
+Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('ticket.details');
